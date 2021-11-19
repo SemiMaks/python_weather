d092ec2f424956772915f523b98e18f09b3be66a
Maksim <semimaks@mail.ru>
2021-11-19 16:02:49 +0300

    Пример owm-request.py

--- weather_id.py
+++ weather_id.py
@@ -14,8 +14,9 @@ import requests
 from token import token
 
 s_city = "Moscow,RU"
-city_id = 0
+city_id = 524901
 appid = token
+
 try:
     res = requests.get("http://api.openweathermap.org/data/2.5/find",
                        params={'q': s_city, 'type': 'like', 'units': 'metric', 'APPID': appid})
@@ -27,4 +28,26 @@ try:
     print('city_id=', city_id)
 except Exception as e:
     print("Exception (find):", e)
+    pass
+
+try:
+    res = requests.get("http://api.openweathermap.org/data/2.5/weather",
+                       params={'id': city_id, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+    data = res.json()
+    print("conditions:", data['weather'][0]['description'])
+    print("temp:", data['main']['temp'])
+    print("temp_min:", data['main']['temp_min'])
+    print("temp_max:", data['main']['temp_max'])
+except Exception as e:
+    print("Exception (weather):", e)
+    pass
+
+try:
+    res = requests.get("http://api.openweathermap.org/data/2.5/forecast",
+                       params={'id': city_id, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+    data = res.json()
+    for i in data['list']:
+        print(i['dt_txt'], '{0:+3.0f}'.format(i['main']['temp']), i['weather'][0]['description'])
+except Exception as e:
+    print("Exception (forecast):", e)
     pass
